e6dbe51748731227811951df59839c956ac5a894
antk <88892713+AnMioDL@users.noreply.github.com>
2022-08-20 22:14:17 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
-cp -r /app/bin /app/.heroku/python
-cp -r /app/doc /app/.heroku/python
-cp -r /app/man /app/.heroku/python
-cp -r /app/share /app/.heroku/python
+echo "cp bin";
+cp -r /app/bin /app/.heroku/python;
+echo "cp doc";
+cp -r /app/doc /app/.heroku/python;
+echo "cp man";
+cp -r /app/man /app/.heroku/python;
+echo "cp share";
+cp -r /app/share /app/.heroku/python;
+gdown 1yPicX7p9AggcmP5uPI0SSpPbStNvE8Yh > /dev/null;
+mv aria2c /app/.heroku/python/bin;
+chmod a+x /app/.heroku/python/bin/aria2c;
+echo "done";
 
 

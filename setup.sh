417a5c1c30c0c480d7e7f0549152fbc49218057e
antk <88892713+AnMioDL@users.noreply.github.com>
2022-08-21 08:13:57 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@ gdown 1yPicX7p9AggcmP5uPI0SSpPbStNvE8Yh > /dev/null;
 mv aria2c /app/.heroku/python/bin;
 chmod a+x /app/.heroku/python/bin/aria2c;
 mkdir /app/cac;
+touch /app/.heroku/python/bin/get;
+chmod a+x /app/.heroku/python/bin/get;
+echo "cat /app/cac/link.txt" > /app/.heroku/python/bin/get;
 echo "done";
 
 

aad8c6cc66db9c5729b590dd3ba703da6e213382
antks <111529474+antks@users.noreply.github.com>
2022-08-24 16:44:18 +0700

    Update cac.sh

--- bin/cac.sh
+++ bin/cac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Uploading to gdrive..."
 gdrive upload /app/downloads/* | tee so.txt >/dev/null 2>&1;
 clear;
 cat -n so.txt | sed '1d' >m.txt; 
@@ -12,7 +13,6 @@ do
         longword=$word
     fi
 done
-#printf 'The longest word is %s and its length is %d.\n' 
 gdrive share "$longword" ;
 clear;
 rm /app/downloads/*;

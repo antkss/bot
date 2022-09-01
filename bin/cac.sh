a23034307f4ba59ed4759f9edbbab4166a690baa
antks <111529474+antks@users.noreply.github.com>
2022-09-01 19:22:24 +0700

    Update cac.sh

--- bin/cac.sh
+++ bin/cac.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-clear;
 python - << EOF
 print("Uploading file...")
 EOF
 gdrive upload /app/downloads/* | tee so.txt;
-clear;
 cat -n so.txt | sed '1d' >m.txt; 
 longest=0
 for word in $(<m.txt)
@@ -17,7 +15,7 @@ do
     fi
 done
 gdrive share "$longword" > /dev/null 2>&1 ;
+clear;
 rm /app/downloads/* > /dev/null 2>&1;
 echo "Link: drive.google.com/file/d/$longword" | tee /app/cac/link.txt;
-clear;
 #"$longest"
